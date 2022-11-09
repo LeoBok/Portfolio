dddcf623239c79ebc32c05dfe5130d7bfbcf8c0a
Leonardo Boccali <leonardoboccali19@gmail.com>
2022-11-09 19:48:32 +0100

    completingg main section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [darkMode, setDarkMode] = useState(false)
   return (
     <div className={darkMode ? "dark" : ""}>
-      <div className='bg-slate-50 text-gray-900 dark:bg-gray-900 dark:text-slate-50 py-6 px-5 h-screen'>
+      <div className='bg-slate-50 text-gray-900 dark:bg-gray-900 dark:text-slate-50 py-6 px-5'>
         <header>
           <Navbar
             darkMode={darkMode}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,20 +1,26 @@
 import { 
     FaLinkedin,
     FaGithub,
-    FaInstagram
+    FaInstagram,
+    FaHtml5,
+    FaCss3Alt,
+    FaReact,
+    FaSass
 } from "react-icons/fa";
+import { SiJavascript, SiTailwindcss } from "react-icons/si";
+import Avatar from "../../utilities/images/avatar.png"
 
 const Main = () => {
     return (
-        <main className="text-center pt-20 px-4">
-            <h2 className="text-5xl text-teal-500">Leonardo Boccali</h2>
-            <h3 className="text-2xl">Jr. Frontend Developer</h3>
-            <p className="text-md leading-8">
+        <main className="text-center pt-16 px-4">
+            <h2 className="text-5xl text-teal-500 py-7">Leonardo Boccali</h2>
+            <h3 className="text-2xl py-5">Jr. Frontend Developer</h3>
+            <p className="text-sm leading-8">
                 I'm a Jr. Frontend Developer 🧑🏻‍💻 <br />
                 I love creating simple but effective UX/UI interfaces 👨🏻‍🎨 <br />
                 Experimenting with new technologies is a must 🧑🏻‍🚀
             </p>
-            <section className="text-4xl flex justify-center items-center gap-14">
+            <section className="text-4xl pt-14 flex justify-center items-center gap-14">
                 <a href="https://www.linkedin.com/in/leonardoboccali/" target="_blank" rel="noreferrer">
                     <FaLinkedin />
                 </a>
@@ -24,6 +30,22 @@ const Main = () => {
                 <a href="https://www.instagram.com/_leo_bok/?next=%2F" target="_blank" rel="noreferrer">
                     <FaInstagram />
                 </a>
+            </section>
+            <img
+                src={Avatar}
+                alt="My own avatar"
+                className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-40 h-40 object-cover mt-10"
+            />
+            <section className="py-10">
+                <h2 className="text-2xl">Technologies that I know:</h2>
+                <ul className="text-4xl py-5 grid grid-cols-2 grid-rows-2 place-content-center gap-y-5">
+                    <li className="mx-auto p-5 shadow-lg dark:shadow-black"> <FaHtml5 /> </li>
+                    <li className="mx-auto p-5 shadow-lg dark:shadow-black"> <FaCss3Alt /> </li>
+                    <li className="mx-auto p-5 shadow-lg dark:shadow-black"> <SiTailwindcss /> </li>
+                    <li className="mx-auto p-5 shadow-lg dark:shadow-black"> <FaSass /> </li>
+                    <li className="mx-auto p-5 shadow-lg dark:shadow-black"> <SiJavascript /> </li>
+                    <li className="mx-auto p-5 shadow-lg dark:shadow-black"> <FaReact /> </li>
+                </ul>
             </section>
         </main>
     )
