50506e5de8b6b34cb11b54aa08eb518c00e94539
Leonardo Boccali <leonardoboccali19@gmail.com>
2022-11-15 11:45:51 +0100

    fixed padding in Works component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,9 +2,10 @@ import {
     FaHtml5,
     FaCss3Alt,
     FaReact,
-    FaSass
+    FaSass,
+    FaGithub
 } from "react-icons/fa";
-import { SiJavascript, SiTailwindcss } from "react-icons/si";
+import { SiJavascript, SiTailwindcss, SiWebpack } from "react-icons/si";
 import { BiCodeCurly } from "react-icons/bi";
 
 const Experience = () => {
@@ -15,13 +16,15 @@ const Experience = () => {
                 <h2 className="pl-3">Frontend developer experience:</h2>
             </div>
                     
-            <ul className="text-4xl py-5 grid grid-cols-3 grid-rows-2 gap-7 place-content-center">
+            <ul className="text-4xl py-5 grid grid-cols-3 grid-rows-3 gap-7 place-content-center">
                 <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <FaHtml5 /> </li>
                 <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <FaCss3Alt /> </li>
+                <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <SiJavascript /> </li>
                 <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <SiTailwindcss /> </li>
                 <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <FaSass /> </li>
-                <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <SiJavascript /> </li>
                 <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <FaReact /> </li>
+                <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <FaGithub /> </li>
+                <li className="p-5 mx-auto shadow-lg dark:shadow-black rounded-md"> <SiWebpack /> </li>
             </ul>
         </section>
   )
